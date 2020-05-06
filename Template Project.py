ba8660e2ee61d36d43570890955ddd192e16e5ec
Menna Mohie <mennaxmohiex@gmail.com>
2020-05-06 03:35:47 +0200

    Hough Transform and its test implemented

--- Template Project.py
+++ Template Project.py
@@ -1,17 +1,21 @@
+from tqdm import trange
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-import matplotlib.colors as mpcol
 import numpy as np
 import glob
 import math
-import cv2
 from scipy import interpolate
-from PIL import Image
+from PIL import Image, ImageDraw
 from scipy import ndimage
+import cv2
 
 
-team_members_names = ['اسم الطالب باللغة العربية', 'ميرنا محمد يسري', 'إسراء ياسر ابوالقاسم']
-team_members_seatnumbers = ['رقم الجلوس', '2016170450', '2016170080']
+team_members_names = ['إسراء ياسر ابوالقاسم',
+                      'منة محيي الدين محمود',
+                      'ميرنا محمد يسري']
+team_members_seatnumbers = ['2016170080',
+                            '2016170438',
+                            '2016170450']
 
 
 def draw_lines_connected(img, lines, color=[255, 0, 0], thickness=8):
@@ -33,6 +37,7 @@ def convert_rbg_to_grayscale(img):
 def convert_rgb_to_hsv(img):
     # This function will do color transform from RGB to HSV
     return cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+
 
 
 def gaussian_kernel(sigma=0.5):
@@ -145,6 +150,7 @@ def double_thresholding(img, low_threshold=2, high_threshold=25):
 
     return img, weak, strong
 
+
 def hysteresis_edge_tracking(img, weak, strong):
     height, width = img.shape
     # Get the indices of the weak pixels
@@ -176,26 +182,26 @@ def detect_edges_canny(img, low_threshold, high_threshold):
     # 1. Noise reduction
     # using Gaussian Smoothing Kernel
     img = noise_reduction(img)
-    #plotting(img, 1, 'Gaussian Filter')
+    plotting(img, 1, 'Gaussian Filter')
 
     # 2. Gradient Calculations
     # using Sobel kernels
     img, theta = gradient_calculation(img)
-    #plotting(img, 2, 'Sobel Filter (G)')
+    plotting(img, 2, 'Sobel Filter (G)')
 
     # 3. Non-Maximum Suppression
     img = non_max_suppression(img, theta)
-    #plotting(img, 3, 'Non-Maximum Suppression')
+    plotting(img, 3, 'Non-Maximum Suppression')
 
     # 4. Double Thresholding
     img, weak, strong = double_thresholding(img, low_threshold, high_threshold)
-    #plotting(img, 4, 'Double Thresholding')
+    plotting(img, 4, 'Double Thresholding')
 
     # 5. Edge Tracking by Hysteresis
     img = hysteresis_edge_tracking(img, weak, strong)
-    #plotting(img, 5, 'Edge Tracking by Hysteresis')
+    plotting(img, 5, 'Edge Tracking by Hysteresis')
 
-    #plt.show()
+    plt.show()
     return img
 
 
@@ -209,14 +215,84 @@ def mask_image(img, vertices):
     return
 
 
-def hough_transform():
-    # Parameters to be determined
-    # Apply Hough transform to find the lanes
+# Apply Hough transform to find the lanes
+# Input:
+#   img - numpy array: binary image containing the edges after applying Canny Detector
+# Output:
+#   lines - list of list: list of lines, each line = [x1, y1, x2, y2]
+def hough_transform(img, accepted_ratio=0.5, rho_step=1, theta_step=1):
+    print('\nHough Transform in progress')
+    # Calculating diagonal length of the image to define the range of the rhos
+    height, width = img.shape
+    diagonal_length = round(math.sqrt(height ** 2 + width ** 2))
+    rhos = np.arange(-diagonal_length, diagonal_length, step=rho_step)
+
+    # Setting the thetas and their cosines/sines
+    thetas = np.arange(-90.0, 90.0, step=theta_step)
+    cos_thetas = np.cos(np.deg2rad(thetas))
+    sin_thetas = np.sin(np.deg2rad(thetas))
+
+    accumulator = np.zeros((len(rhos), len(thetas)))
+
+    edge_pts_ys, edge_pts_xs = np.nonzero(img)
+    for i in trange(len(edge_pts_xs)):
+        x = edge_pts_xs[i]
+        y = edge_pts_ys[i]
+        for theta_idx in range(len(thetas)):
+            rho = x * cos_thetas[theta_idx] + y * sin_thetas[theta_idx]
+            rho_idx = np.argmin(np.abs(rhos - rho))  # find the index of the closest rho to the computed rho
+            accumulator[rho_idx, theta_idx] += 1
+
+    # lines_rhos = []
+    # lines_thetas = []
+    lines_rhos_indices, lines_thetas_indices = np.nonzero(accumulator >= accumulator.max() * accepted_ratio)
+    lines = []
+    for i in range(len(lines_rhos_indices)):
+        rho_idx = lines_rhos_indices[i]
+        rho = rhos[rho_idx]
+        # lines_rhos.append(rho)
+
+        theta_idx = lines_thetas_indices[i]
+        theta = thetas[theta_idx]
+        # lines_thetas.append(theta)
+
+        a = cos_thetas[theta_idx]
+        b = sin_thetas[theta_idx]
+        x0 = a * rho
+        y0 = b * rho
+        x1 = int(x0 + 10000 * (-b))
+        y1 = int(y0 + 10000 * a)
+        x2 = int(x0 - 10000 * (-b))
+        y2 = int(y0 - 10000 * a)
+        lines.append([x1, y1, x2, y2])
+    return lines
+
+
+def draw_hough_lines(img, lines, path, color=[255, 0, 0], thickness=8):
+    d = ImageDraw.Draw(img)
+    for line in lines:
+        d.line(line, fill=(255, 255, 255), width=thickness)
+        d.line(line, fill=(color[0], color[1], color[2]), width=thickness)
+    img.save(path)
     return
 
 
+def hough_test():
+    input_image = read_image('hough_test_1.jpg')
+    edges = detect_edges_canny(input_image, 2, 50)
+    lines = hough_transform(edges, 0.3)
+    draw_hough_lines(Image.open('hough_test_1.jpg'), lines, 'hough_test_1_out.jpg')
+
+    input_image = read_image('hough_test_2.jpg')
+    edges = detect_edges_canny(input_image, 2, 50)
+    lines = hough_transform(edges)
+    draw_hough_lines(Image.open('hough_test_2.jpg'), lines, 'hough_test_2_out.jpg')
+    return
+
 def read_image(img_path):
-    img = mpimg.imread(img_path)
+    img = Image.open(img_path)
+    img = img.convert("L")
+    img = np.asarray(img, dtype=int)
     return img
 
 
@@ -224,8 +300,8 @@ def read_video(vid_path):
     # TODO: implement this function
     return  # video_array
 
-def color_thresholding(img, low_threshold, high_threshold):
 
+def color_thresholding(img, low_threshold, high_threshold):
     # define the fixed values of the pixels
     strong = 255
     outimg = np.zeros(img.shape)
@@ -240,53 +316,57 @@ def color_thresholding(img, low_threshold, high_threshold):
 
     return outimg
 
+
 # main part
 def main():
     # TODO: the below lines are for initial testing, remove them later
-    #input_image = read_image('TestCanny.jpg')
-    #img = detect_edges_canny(input_image, low_threshold=2, high_threshold=50)
-    #return
+    # input_image = read_image('TestCanny.jpg')
+    # img = detect_edges_canny(input_image, low_threshold=2, high_threshold=50)
+    # return
 
     # 1. read the image
     input_img = read_image('lanes_test.jpg')
 
     # 2. convert to HSV
     HSV_img = convert_rgb_to_hsv(input_img)
-    
+
     # 3. convert to Gray
     gray_img = convert_rbg_to_grayscale(input_img)
-    
+
     # 4. Threshold HSV for Yellow and White (combine the two results together)
 
     # 4.1 Threshold HSV for Yellow
-    lower = np.uint8([10,   0, 200])
+    lower = np.uint8([10, 0, 200])
     upper = np.uint8([40, 255, 255])
     yellow_mask = color_thresholding(HSV_img, lower, upper)
 
     # 4.1 Threshold HSV for White
-    lower = np.uint8([  0,   0, 200])
+    lower = np.uint8([0, 0, 200])
     upper = np.uint8([255, 55, 255])
     white_mask = color_thresholding(HSV_img, lower, upper)
 
     # 4.1 Combine the resuls together
     mask = cv2.bitwise_or(white_mask, yellow_mask)
-    
+
     # 5. Mask the gray image using the threshold output from step 4
-    #I'm not sure about which masking they're asking for TBH
+    # I'm not sure about which masking they're asking for TBH
     masked_gray_img = np.zeros(gray_img.shape)
-    cv2.bitwise_and(gray_img, masked_gray_img, mask = mask)
-    
+    cv2.bitwise_and(gray_img, masked_gray_img, mask=mask)
+
     # 6. Apply noise remove (gaussian) to the masked gray image
     masked_gray_img = remove_noise(masked_gray_img, 3)
-    
+
     # 7. use canny detector and fine tune the thresholds (low and high values)
     canny_out_img = detect_edges_canny(masked_gray_img, low_threshold=2, high_threshold=50)
-    
+
     # 8. mask the image using the canny detector output
+
     # 9. apply hough transform to find the lanes
+
     # 10. apply the pipeline you developed to the challenge videos
 
     # 11 You should submit your code
 
 
+# hough_test()
 main()
